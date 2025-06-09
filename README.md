a39af2431c48a405e9eaf7694a719005fcbfb8ef
GW_Sarah <sarah@GW-Sarahui-MacBookPro.local>
2025-06-09 13:41:19 +0900

    feat: [#1] 팀 예시 코드 추가 및 리드미 업뎃

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# code-style
+# FE code-style
 
-FE 내 공통 코드 스타일 설정 관련 파일 모음
+프론트엔드 팀의 통일된 코드 스타일을 위한 설정 관련 파일 모음입니다
 
-## 파일 구조
+## 설정 파일 종류 및 폴더 구조
+
+### 1. .editorconfig
+
+### 2. ESLint
+
+### 3. Prettier
+
+## IDE 관련
+
+## ✅ VSCode 권장 버전
+
+- 권장 버전: `>=1.85.0`
+- 확인 명령어: `code --version`
+
+## 설정 파일 외에 살펴 보아야 할 환경 세팅
+
+IDE 별로 코드 스타일 관련 세팅 확인 필요
+
+- vscode > 워크스페이스 (.vscode/settings.json) > "editor.formatOnSave" 등 예시 확인
+- Jetbrains계열 > Preferences > languages & Framworks > javascript > Code Quality Tools > ESLint 설정 파일 경로 지정 | Prettier on save, configuration file 등
--- eslint/.eslintrc.cjs
+++ eslint/.eslintrc.cjs
@@ -25,18 +25,27 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended",
+    "prettier", // prettier 충돌 방지
   ],
   rules: {
-    "unused-imports/no-unused-imports": "error",
+    // 팀 선호 기반
     "no-console": ["warn", { allow: ["warn", "error"] }],
-    "no-debugger": "error",
-    "@typescript-eslint/consistent-type-imports": "warn",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "react/jsx-uses-react": "off",
-    "react-hooks/exhaustive-deps": "warn",
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "react/react-in-jsx-scope": "off", // React 17+
+    "react/prop-types": "off", // TypeScript 사용 시 불필요
+    "@typescript-eslint/consistent-type-imports": "warn", // 일반 import로 타입만 가져옴 경고
+    "@typescript-eslint/no-explicit-any": "warn", // any 사용 억제
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], // 안쓰는 변수 방지, 언더바로 시작 시 허용
+
+    // 가독성 관련
+    "object-curly-spacing": ["error", "always"], // import { a, b } 공백 선호
+    "comma-dangle": ["error", "only-multiline"], // 여러줄 구조에서만 마지막 쉼표 강제, 단일줄에서는 쉼표x
+    indent: ["error", 2, { SwitchCase: 1 }], // 들여쓰기 2칸
+    quotes: ["error", "single", { avoidEscape: true }], // 기본 따옴표 싱글, 단 문자열 안에 이미 싱글 쓰면 더블 인정
+    semi: ["error", "always"], // 세미콜론 강제
+
+    // 그 외 권장
+    "react-hooks/exhaustive-deps": "warn", // useXX React훅 의존성 배열 누락 경고
   },
   settings: {
     react: {
